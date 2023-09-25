7719b2c2917fec4393a9ba06c99065414fa20efe
Michael Wijaya Sutrisna <michaelwijayasutrisna2002@gmail.com>
2023-09-25 14:46:25 +0700

    Update BLE_iBeacon.ino

--- BLE_iBeacon.ino
+++ BLE_iBeacon.ino
@@ -17,12 +17,17 @@
 #include <BLEUtils.h>
 #include <BLE2902.h>
 #include <BLEBeacon.h>
+#include <FastLED.h>
 
 #define DEVICE_NAME            "ESP32"
 #define SERVICE_UUID           "5A4BCFCE-174E-4BAC-A814-092E77F6B7E5"
 #define BEACON_UUID            "2D7A9F0C-E0E8-4CC9-A71B-A21DB2D034A1" // Yang dipake nanti
 #define BEACON_UUID_REV        "A134D0B2-1DA2-1BA7-C94C-E8E00C9F7A2D"
 #define CHARACTERISTIC_UUID    "82258BAA-DF72-47E8-99BC-B73D7ECD08A5"
+
+#define DATA_PIN 5
+
+CRGB leds[1];
 
 BLEServer *pServer;
 BLECharacteristic *pCharacteristic;
@@ -117,6 +122,9 @@ void setup() {
   Serial.println("Initializing...");
   Serial.flush();
 
+  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, 1);
+  FastLED.setBrightness(50);
+
   BLEDevice::init(DEVICE_NAME);
   pServer = BLEDevice::createServer();
   pServer->setCallbacks(new MyServerCallbacks());
@@ -126,7 +134,11 @@ void setup() {
 
   Serial.println("iBeacon + service defined and advertising!");
 
-  pinMode(16, OUTPUT);
+  // TTGO =============================
+  // pinMode(16, OUTPUT);
+
+  // Firebeetle blink red ===================================================
+  pinMode(2, OUTPUT);
 }
 
 void loop() {
@@ -137,9 +149,21 @@ void loop() {
     value++;
   // }
 
-  digitalWrite(16, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(500);                      // wait for a second
-  digitalWrite(16, LOW);   // turn the LED off by making the voltage LOW
-  delay(500);   
+  // TTGO =============================
+  // digitalWrite(16, HIGH);  // turn the LED on (HIGH is the voltage level)
+  // delay(500);                      // wait for a second
+  // digitalWrite(16, LOW);   // turn the LED off by making the voltage LOW
+  // delay(500);   
   // delay(2000);
+
+  // Firebeetle blink red ===================================================
+  digitalWrite(2, HIGH);  // turn the LED on (HIGH is the voltage level)
+  leds[0] = CRGB::Red;
+  FastLED.show();
+  delay(500);                      // wait for a second
+  digitalWrite(2, LOW);   // turn the LED off by making the voltage LOW
+  leds[0] = CRGB::Black;
+  FastLED.show();
+  delay(500); 
+   
 }
